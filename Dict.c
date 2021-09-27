9e35c1a053fbc2804cb82d26923956065ab3ad08
zachjbrowning <zachjbrowning@outlook.com>
2021-09-27 21:10:13 +1000

    Part 2 donesky

--- Dict.c
+++ Dict.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "Dict.h"
 #include "BST.h"
 
 #define MAX 8192
+
+static bool isSuffix(char *suffix, char *msg);
 
 Dict buildDict(FILE *file) {
     Dict tree = NULL;
@@ -12,7 +15,7 @@ Dict buildDict(FILE *file) {
     char translation[MAX];
     char verb[MAX];
 
-    while (fscanf(file, "%s", verb) != EOF) {
+    while (fscanf(file, "%s ", verb) != EOF) {
 
         fgets(translation, MAX, file);
 
@@ -28,8 +31,19 @@ Dict buildDict(FILE *file) {
 
 // PART 2
 void searchDict(Dict dict, char *query) {
-    return;
+    if (dict == NULL) return;
+
+    int cmp = strcmp(query, dict->verb);
+    if (cmp > 0) 
+        return;
+    else if (cmp < 0 )
+        searchDict(dict->left, query);
+
+    if (isSuffix(query, dict->verb))
+        printf("%s %s\n", dict->verb, dict->translation);
 }
+
+
 
 // PART 3
 void listFrequent(Dict dict) {
@@ -38,5 +52,21 @@ void listFrequent(Dict dict) {
 
 // ALSO NEED IMPLEMENTING
 void freeDict(Dict dict) {
-    return;
+    freeBST(dict);
+}
+
+
+/*
+        HELPERS
+*/
+
+
+static bool isSuffix(char *suffix, char *msg) {
+    int suffixLen = strlen(suffix);
+    int msgLen = strlen(msg);
+    if (suffixLen > msgLen) return false;
+    for (int i = 0; i < suffixLen; i++) {
+        if (suffix[i] != msg[i]) return false;
+    }
+    return true;
 }
--- practice.c
+++ practice.c
@@ -7,16 +7,17 @@
 
 
 int main(int argc, char **argv) {
+    char suffix[10] = "a";
+    char msg[10] = "suffix";
+    // Node tree = insertNode(NULL, "premiere", "firstly");
 
-    Node tree = insertNode(NULL, "premiere", "firstly");
+    // tree = insertNode(tree, "deuxieme", "secondly");
+    // tree = insertNode(tree, "ameliorer", "to improve idk");
+    // tree = insertNode(tree, "bouger", "to move");
+    // tree = insertNode(tree, "courir", "to run");
+    // tree = insertNode(tree, "degager", "to get out of the way");
 
-    tree = insertNode(tree, "deuxieme", "secondly");
-    tree = insertNode(tree, "ameliorer", "to improve idk");
-    tree = insertNode(tree, "bouger", "to move");
-    tree = insertNode(tree, "courir", "to run");
-    tree = insertNode(tree, "degager", "to get out of the way");
-
-    printBST(tree);
-
+    // printBST(tree);
+    printf("%d\n", strcmp(suffix, msg));
     return 0;
 }
